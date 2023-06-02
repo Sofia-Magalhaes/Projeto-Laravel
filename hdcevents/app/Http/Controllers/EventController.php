c91b673fa8ebfb44875dd05379a9a6fa9083e2c3
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-06-02 15:17:35 -0300

    Busca no Laravel

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -9,11 +9,20 @@ class EventController extends Controller
 {
     public function index()
     {
-        $events = Event::all();
+        $search = request('search');
+
+        if ($search) {
+            $events = Event::where([
+                ['title', 'like', '%' . $search . '%'],
+            ])->get();
+        } else {
+            $events = Event::all();
+        }
+
 
         return view(
             'welcome',
-            ['events' => $events]
+            ['events' => $events, 'search' => $search]
         );
     }
 
