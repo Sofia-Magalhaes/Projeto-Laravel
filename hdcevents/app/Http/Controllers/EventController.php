1e6d9bca6237303ebb0d6f9fa5fa853955efaadf
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2023-06-05 14:42:33 -0300

    Removendo presença do evento
    Cancelar presença do evento e mostrar mensagem quando usuário estiver participando

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -71,9 +71,22 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if ($user) {
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach ($userEvents as $userEvent) {
+                if ($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard()
@@ -103,7 +116,7 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        if($user->id != $event->user_id){
+        if ($user->id != $event->user_id) {
             return redirect('/dashboard');
         }
 
@@ -144,5 +157,16 @@ class EventController extends Controller
         $event = Event::findOrFail($id);
 
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento ' . $event->title);
     }
 }
